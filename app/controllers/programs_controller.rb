a15cf87d2b50859e531c46926fb631d4d0188bdf
Umair-ah <umairahmed200203.umair@gmail.com>
2024-11-28 11:52:46 +0530

    better ui and seperate login and marks allot for guides even as reviewers

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,7 +2,12 @@ class ProgramsController < ApplicationController
 
   def show  
     @program = Program.find(params[:id])
-    @projects = @program.projects.joins(:students).where(students: { guide_id: session[:guide_id] }).uniq
+
+    if session[:user_category] == "Guide"
+      @projects = @program.projects.joins(:students).where(students: { guide_id: session[:guide_id] }).uniq
+    else
+      @projects = Project.all.order(created_at: :desc)
+    end
 
   end
 end
